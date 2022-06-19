a2e4375dbcfb83449f4e144d08e5c00b4d11c415
lukinoo <luka.bliadze.2015@mail.ru>
2022-06-19 17:24:04 +0400

    init: Project-setup 🎉

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="hello world" :counter="counter" />
+  <button v-on:click="increment()">increment</button>
+  <button v-on:click="decrement()">decrement</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter += 1;
+    },
+    decrement() {
+      this.counter -= 1;
+    },
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h3 v-bind:class="counter! % 2 === 0 ? 'even' : 'odd'">
+      count: {{ counter }}
+    </h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+    counter: Number,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+h3.even {
+  color: #42b983;
+}
+h3.odd {
+  color: #8a2be2;
+}
+</style>
