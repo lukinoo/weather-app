552edff12405d2d24c77378d64d89e9325862987
lukinoo <luka.bliadze.2015@mail.ru>
2022-06-19 17:46:19 +0400

    chore: less-styles

--- src/App.vue
+++ src/App.vue
@@ -30,13 +30,7 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
